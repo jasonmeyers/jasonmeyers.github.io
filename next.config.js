77d67eb7b61f0479e1199134e210685f15a13a77
Jason Meyers <jason.meyers@gmail.com>
2024-01-17 17:38:14 -0800

    adding config changes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  // basePath: "/jasonmeyers",
+  basePath: "/jasonmeyers.github.io",
   images: {
     unoptimized: true,
   },
