dff7ea42f43c6d65eaa8fa109c76f33fee0df412
Jason Meyers <jason.meyers@gmail.com>
2024-01-17 23:39:43 -0800

    adding mugshot

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Image from "next/image";
+import jasonImage from "../../public/jason-meyers.png";
+import styles from "../app/page.module.css";
 
 function Introduction() {
   return (
     <div className="description">
-      <h1>I am Jason Meyers</h1>I work as a <strong>Web Developer</strong> and{" "}
-      <strong>UI Engineer</strong>.
+      <Image src={jasonImage} alt="Jason Meyers" width={400} />
+      <h1 className={styles.h1}>I am Jason Meyers</h1>I work as a{" "}
+      <strong>Web Developer</strong> and <strong>UI Engineer</strong>.
       <p>
         I build UI + help organizations explore, define, and create products
       </p>
