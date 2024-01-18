233c86adebb828625dd19cbd8e3c916c900919f5
Jason Meyers <jason.meyers@gmail.com>
2024-01-18 15:51:39 -0800

    fixing CSS for mobile

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -6,13 +6,13 @@ import styles from "../app/page.module.css";
 function Introduction() {
   return (
     <div className={styles.description}>
-      <Image src={jasonImage} alt="Jason Meyers" width={600} />
+      <Image src={jasonImage} alt="Jason Meyers" width={600} priority />
       <h1 className={styles.h1}>I am Jason Meyers</h1>
       <p>
-        I work as a <strong>Design </strong> + <strong>UI Engineer</strong>. I
-        build cloud-native SaaS products, design systems, and high-touch
-        information interfaces - from Finance to Education, Insurance to
-        Publishing.
+        I work as a <strong>Design </strong> + <strong>UI Engineer</strong>{" "}
+        &mdash; I build cloud-native SaaS products, design systems, and
+        high-touch information interfaces &mdash; from Finance to Education,
+        Insurance to Publishing.
       </p>
       <p>
         <a
