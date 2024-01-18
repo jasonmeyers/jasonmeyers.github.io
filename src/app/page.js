aad2172641bddab09268a146dfa51afbbb6373bf
Jason Meyers <jason.meyers@gmail.com>
2024-01-17 17:50:08 -0800

    adding components

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,11 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Introduction from "@/components/Introduction";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className="description">
-        <h1>I am Jason Meyers</h1>I work as a <strong>Web Developer</strong> and{" "}
-        <strong>UI Engineer</strong>.
-        <p>
-          I build UI + help organizations explore, define, and create products
-        </p>
-        <p>
-          <a
-            href="https://www.linkedin.com/in/jason-meyers-ui/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Linkedin
-          </a>{" "}
-          <a
-            href="https://github.com/jasonmeyers"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Github
-          </a>
-        </p>
-      </div>
+      <Introduction />
     </main>
   );
 }
---
+++ src/components/Introduction.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+function Introduction() {
+  return (
+    <div className="description">
+      <h1>I am Jason Meyers</h1>I work as a <strong>Web Developer</strong> and{" "}
+      <strong>UI Engineer</strong>.
+      <p>
+        I build UI + help organizations explore, define, and create products
+      </p>
+      <p>
+        <a
+          href="https://www.linkedin.com/in/jason-meyers-ui/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linkedin
+        </a>{" "}
+        <a
+          href="https://github.com/jasonmeyers"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github
+        </a>
+      </p>
+    </div>
+  );
+}
+
+export default Introduction;
